49ac9fb49ead0d0a06f7f09cb00fff766ab0e886
Johnathan Lee <john.lee@thediymaker.com>
2025-03-13 14:27:33 -0700

    updating to show only running jobs

--- app/api/slurm/jobs/node/[...id]/route.ts
+++ app/api/slurm/jobs/node/[...id]/route.ts
@@ -6,7 +6,7 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   const res = await fetch(
-    `http://${env.SLURM_SERVER}:6820/slurmdb/${env.SLURM_API_VERSION}/jobs?node=${params.id[0]}&state.current=running`,
+    `http://${env.SLURM_SERVER}:6820/slurmdb/${env.SLURM_API_VERSION}/jobs?node=${params.id[0]}&state=running`,
     {
       headers: {
         "X-SLURM-USER-NAME": `${env.SLURM_API_ACCOUNT}`,
