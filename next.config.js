61a0b8966e56dfebe852f589149dae9f6d2958b2
Johnathan Lee <john.lee@thediymaker.com>
2025-05-07 13:21:14 -0700

    fixing static page generation for api routes

--- next.config.js
+++ next.config.js
@@ -1,30 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "webapp4.asu.edu",
-      },
-    ],
-  },
-  publicRuntimeConfig: {
-    // remove private variables from processEnv
-    processEnv: Object.fromEntries(
-      Object.entries(process.env).filter(([key]) =>
-        key.includes("NEXT_PUBLIC_")
-      )
-    ),
-  },
-};
+    output: "standalone",
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'webapp4.asu.edu'
+            }
 
-module.exports = nextConfig;
+        ],
+    },
+    publicRuntimeConfig: {
+        // remove private variables from processEnv
+        processEnv: Object.fromEntries(
+            Object.entries(process.env).filter(([key]) =>
+                key.includes('NEXT_PUBLIC_')
+            )
+        ),
+    },
+}
+
+module.exports = nextConfig
