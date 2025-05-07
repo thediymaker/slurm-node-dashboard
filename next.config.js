de9435db1521481a3a0c799d2d2ecbf90a677fb7
Johnathan Lee <john.lee@thediymaker.com>
2025-05-07 12:33:37 -0700

    stop api routes from being statically generated

--- next.config.js
+++ next.config.js
@@ -1,31 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com'
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com'
-            },
-            {
-                protocol: 'https',
-                hostname: 'webapp4.asu.edu'
-            }
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "webapp4.asu.edu",
+      },
+    ],
+  },
+  publicRuntimeConfig: {
+    // remove private variables from processEnv
+    processEnv: Object.fromEntries(
+      Object.entries(process.env).filter(([key]) =>
+        key.includes("NEXT_PUBLIC_")
+      )
+    ),
+  },
+};
 
-        ],
-    },
-    publicRuntimeConfig: {
-        // remove private variables from processEnv
-        processEnv: Object.fromEntries(
-            Object.entries(process.env).filter(([key]) =>
-                key.includes('NEXT_PUBLIC_')
-            )
-        ),
-    },
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
